37de9f18499f4d0ef34b905a56c6d0edc2525839
Ali Klein <ali.klein@madeinhaus.com>
2022-11-18 12:56:29 -0800

    Add scss module

--- packages/haus-ui-core/src/Button/index.tsx
+++ packages/haus-ui-core/src/Button/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import styles from "./button.module.scss";
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <button className={styles.root}>{props.children}</button>;
 }
 
 Button.displayName = "Button";
