50206c115edb53d5230a7773f75cd23ab789b280
Ali Klein <ali.klein@madeinhaus.com>
2023-01-03 12:40:18 -0800

    fix vite build

--- packages/haus-ui-core/vite.config.js
+++ packages/haus-ui-core/vite.config.js
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      input: "src/index.ts",
+      output: [
+        {
+          format: "cjs",
+          entryFileNames: "[name].bundle.[format].js",
+        },
+        {
+          format: "esm",
+          entryFileNames: "[name].bundle.[format].js",
+        },
+      ],
+    },
+  },
 }));
