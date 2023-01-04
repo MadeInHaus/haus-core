14c365fb4630ac0f573ccfc47872531b21524c6d
Ali Klein <ali.klein@madeinhaus.com>
2023-01-04 14:11:37 -0800

    Move rollup stuff to dev deps

--- packages/haus-ui-core/rollup.config.js
+++ packages/haus-ui-core/rollup.config.js
@@ -1,4 +1,3 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
@@ -23,7 +22,6 @@ export default {
     },
   ],
   plugins: [
-    peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
