84e10b6f7b565a893162d7d730973752f295987b
Ali Klein <ali.klein@madeinhaus.com>
2023-01-18 16:30:26 -0800

    Revert "link useIntersection from @madeinhaus/core"
    This reverts commit 9538faa39134134290573cd9360055d4b23dc845.

--- packages/core/src/ui/TexturalVideo/index.tsx
+++ packages/core/src/ui/TexturalVideo/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { useIntersection } from '@madeinhaus/core';
+import { useIntersection } from '../../hooks/useIntersection';
 
 export interface TexturalVideoProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
     className?: string;
