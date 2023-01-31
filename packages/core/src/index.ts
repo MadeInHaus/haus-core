79b9377bc70fa9aa198bf5fc598c23d53a886e58
Ali Klein <ali.klein@madeinhaus.com>
2023-01-31 14:35:07 -0800

    Start of portal component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,10 +3,13 @@
 export { Button, type ButtonProps } from './ui/Button';
 export { Masonry, type MasonryProps } from './ui/Masonry';
 export { TexturalVideo, type TexturalVideoProps } from './ui/TexturalVideo';
-export { ContentfulImg, type ContentfulImgProps } from './ui/ContentfulImg';
 export { Carousel, type CarouselProps } from './ui/Carousel';
 
 /* Hooks */
 export { useIsomorphicLayoutEffect } from './hooks/useIsomorphicLayoutEffect';
 export { usePrevious } from './hooks/usePrevious';
 export { useIntersectionObserver } from './hooks/useIntersectionObserver';
+
+/* Misc */
+export { ContentfulImg, type ContentfulImgProps } from './misc/ContentfulImg';
+export { Portal, type PortalProps } from './misc/Portal';
