674ab949a3dbe8405e2b321063da392e3d4b3830
Ali Klein <ali.klein@madeinhaus.com>
2023-02-08 11:37:27 -0800

    remove eslint overrides

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -17,10 +17,4 @@ module.exports = {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
     },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            rules: { 'no-undef': 'off' },
-        },
-    ],
 };
