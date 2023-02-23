622e7fd653c973b6cfee35bc8b3182a7a233078e
Ali Klein <ali.klein@madeinhaus.com>
2023-02-23 15:49:43 -0800

    remove dist from storybook

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -15,7 +15,7 @@ module.exports = {
                 alias: [
                     {
                         find: '@madeinhaus/core',
-                        replacement: path.resolve(__dirname, '../../../packages/core/'),
+                        replacement: path.resolve(__dirname, '../../../packages/core/dist'),
                     },
                     {
                         find: '@madeinhaus/utils',
