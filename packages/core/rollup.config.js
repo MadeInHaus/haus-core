a34f2049c70e41be421304456508a92b57111470
Ali Klein <ali.klein@madeinhaus.com>
2023-02-22 16:18:58 -0800

    Here's your tree shaking

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -4,22 +4,30 @@ import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const getFiles = ext => {
+    return Object.fromEntries(
+        glob.sync(`src/**/*.${ext}`).map(file => [
+            // This remove `src/` as well as the file extension from each
+            // file, so e.g. src/nested/foo.js becomes nested/foo
+            path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+            // This expands the relative paths to absolute paths, so e.g.
+            // src/nested/foo becomes /project/src/nested/foo.js
+            fileURLToPath(new URL(file, import.meta.url)),
+        ])
+    );
+};
+
 export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true,
-            globals: { react: 'React' },
-        },
-        {
-            file: pkg.module,
-            format: 'esm',
-            sourcemap: true,
-            globals: { react: 'React' },
-        },
-    ],
+    input: { ...getFiles('ts'), ...getFiles('tsx') },
+    output: {
+        format: 'esm',
+        dir: 'dist',
+        globals: { react: 'React' },
+    },
     plugins: [
         resolve(),
         commonjs(),
