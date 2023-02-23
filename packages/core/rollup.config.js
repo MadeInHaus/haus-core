42090b9d0c2ab617b454e87d08a62a813e33bb2f
Ali Klein <ali.klein@madeinhaus.com>
2023-02-23 13:47:15 -0800

    rewrite rollup with preserveModules

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,28 +3,13 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 
-import glob from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const getFiles = ext => {
-    return Object.fromEntries(
-        glob.sync(`src/**/*.${ext}`).map(file => [
-            // This remove `src/` as well as the file extension from each
-            // file, so e.g. src/nested/foo.js becomes nested/foo
-            path.relative('src', file.slice(0, file.length - path.extname(file).length)),
-            // This expands the relative paths to absolute paths, so e.g.
-            // src/nested/foo becomes /project/src/nested/foo.js
-            fileURLToPath(new URL(file, import.meta.url)),
-        ])
-    );
-};
-
 export default {
-    input: { ...getFiles('ts'), ...getFiles('tsx') },
+    input: 'src/index.ts',
     output: {
         format: 'esm',
         dir: 'dist',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
         globals: { react: 'React' },
     },
     plugins: [
