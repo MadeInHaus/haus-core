87d05dd7d256332afb049623863b6e6a6c1ebdda
Ali Klein <ali.klein@madeinhaus.com>
2023-02-28 09:59:34 -0800

    Portal to own repo

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,8 +3,3 @@
 export { Masonry, type MasonryProps } from './ui/Masonry';
 export { TexturalVideo, type TexturalVideoProps } from './ui/TexturalVideo';
 export { Carousel, type CarouselProps } from './ui/Carousel';
-
-/* Misc */
-
-export { Portal, type PortalProps } from './misc/Portal';
-0.0;
---
+++ packages/portal/README.md
@@ -0,0 +1,13 @@
+# Portal
+
+**Install**:
+
+```bash
+npm i @madeinhaus/portal
+```
+
+**Usage**:
+
+```javascript
+import Portal from '@madeinhaus/portal';
+```
