d5fa56b2c5a549e5636cc4bfd097ce7cb33c5c47
Ali Klein <ali.klein@madeinhaus.com>
2023-02-28 16:23:22 -0800

    Add eslint-config-haus to other packages

--- packages/hooks/.eslintrc.js
+++ packages/hooks/.eslintrc.js
@@ -1,19 +1,4 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
-    },
-    extends: ['next', 'turbo'],
     root: true,
-    rules: {
-        'import/no-unresolved': 'off',
-        'no-empty': 'off',
-        'no-unused-vars': 'warn',
-        'no-undef': 'warn',
-        'react-hooks/exhaustive-deps': 'warn',
-        'react-hooks/rules-of-hooks': 'error',
-        'react/display-name': 'off',
-    },
+    extends: ['haus'],
 };
