dce6f104c50a30ff2110ad5c6a28bb9710e796c9
Claus Wahlers <claus@codeazur.com.br>
2023-02-28 22:34:52 -0300

    Add exports: 'named' to pagetransition's rollup config

--- packages/nextjs-page-transition/rollup.config.js
+++ packages/nextjs-page-transition/rollup.config.js
@@ -13,12 +13,14 @@ const config = {
             format: 'cjs',
             sourcemap: true,
             globals: { react: 'React' },
+            exports: 'named',
         },
         {
             file: packageJson.module,
             format: 'esm',
             sourcemap: true,
             globals: { react: 'React' },
+            exports: 'named',
         },
     ],
     plugins: [
