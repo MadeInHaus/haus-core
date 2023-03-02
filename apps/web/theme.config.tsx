3843fe54958bff46d36176975f590ed86a05bee1
Claus Wahlers <claus@codeazur.com.br>
2023-03-02 05:05:02 -0300

    Add stubs for all components, hooks and utils

--- apps/web/theme.config.tsx
+++ apps/web/theme.config.tsx
@@ -1,20 +1,27 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>HAUS CORE</span>,
-  project: {
-    link: "https://github.com/MadeInHaus/haus-core-docs",
-  },
-  docsRepositoryBase: "https://github.com/MadeInHaus/haus-core-docs",
-  useNextSeoProps() {
-    return {
-      titleTemplate: "%s – Haus Core Component Library",
-    };
-  },
-  footer: {
-    text: "(c) 2023 Haus LA",
-  },
+    logo: <span>HAUS CORE</span>,
+    project: {
+        link: 'https://github.com/MadeInHaus/haus-core-docs',
+    },
+    docsRepositoryBase: 'https://github.com/MadeInHaus/haus-core-docs',
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Haus Core Component Library',
+        };
+    },
+    feedback: {
+        content: null,
+    },
+    editLink: {
+        component: null,
+    },
+    footer: {
+        component: null,
+    },
+    navigation: false,
 };
 
 export default config;
