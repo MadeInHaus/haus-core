cc76a9f435561aaa6af63a6e145e0d0339715248
Ali Klein <ali.klein@madeinhaus.com>
2023-03-13 09:59:33 -0700

    Disclosure docs cleanup

--- apps/web/components/DisclosureDemo/DisclosureDemo.tsx
+++ apps/web/components/DisclosureDemo/DisclosureDemo.tsx
@@ -1,21 +1,17 @@
 import Disclosure from '@madeinhaus/disclosure';
-
-import disclosureItems from './data/disclosureItems';
-
+import items from './data/items';
 import styles from './DisclosureDemo.module.css';
 
 const DisclosureDemoBasic: React.FC = () => {
   return (
-    <div className={styles.root}>
-      <Disclosure.Root>
-        {disclosureItems.map(({ heading, paragraph }, index) => (
-          <Disclosure.Details key={index}>
-            <Disclosure.Summary>{heading}</Disclosure.Summary>
-            <Disclosure.Content>{paragraph}</Disclosure.Content>
-          </Disclosure.Details>
-        ))}
-      </Disclosure.Root>
-    </div>
+    <Disclosure.Root className={styles.root}>
+      {items.map(({ heading, paragraph }, index) => (
+        <Disclosure.Details key={index}>
+          <Disclosure.Summary>{heading}</Disclosure.Summary>
+          <Disclosure.Content>{paragraph}</Disclosure.Content>
+        </Disclosure.Details>
+      ))}
+    </Disclosure.Root>
   );
 };
 
--- apps/web/components/DisclosureDemo/DisclosureDemoRenderProp.tsx
+++ apps/web/components/DisclosureDemo/DisclosureDemoRenderProp.tsx
@@ -1,30 +1,23 @@
-import * as React from 'react';
-import cx from 'clsx';
-
 import Disclosure from '@madeinhaus/disclosure';
-
-import disclosureItems from './data/disclosureItems';
-
+import items from './data/items';
 import styles from './DisclosureDemoRenderProp.module.css';
 
 const DisclosureDemoRenderProp: React.FC = () => {
   return (
-    <div className={styles.root}>
-      <Disclosure.Root>
-        {disclosureItems.map(({ heading, paragraph }, index) => (
-          <Disclosure.Details key={index}>
-            {({ isOpen }) => (
-              <>
-                <Disclosure.Summary>{`${heading} - ${
-                  isOpen ? 'open' : 'closed'
-                }`}</Disclosure.Summary>
-                <Disclosure.Content>{paragraph}</Disclosure.Content>
-              </>
-            )}
-          </Disclosure.Details>
-        ))}
-      </Disclosure.Root>
-    </div>
+    <Disclosure.Root className={styles.root}>
+      {items.map(({ heading, paragraph }, index) => (
+        <Disclosure.Details key={index}>
+          {({ isOpen }) => (
+            <>
+              <Disclosure.Summary>{`${heading} - ${
+                isOpen ? 'open' : 'closed'
+              }`}</Disclosure.Summary>
+              <Disclosure.Content>{paragraph}</Disclosure.Content>
+            </>
+          )}
+        </Disclosure.Details>
+      ))}
+    </Disclosure.Root>
   );
 };
 
