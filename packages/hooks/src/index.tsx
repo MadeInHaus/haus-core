78a881c0f24e022290fad5c0f2880af3d1ee50fc
Claus Wahlers <claus@codeazur.com.br>
2023-05-13 00:38:20 -0300

    Actually export useRefList

--- packages/hooks/src/index.tsx
+++ packages/hooks/src/index.tsx
@@ -3,3 +3,4 @@ export { useIntersectionObserver } from './useIntersectionObserver';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { usePrevious } from './usePrevious';
 export { useWindowSize } from './useWindowSize';
+export { useRefList } from './useRefList';
