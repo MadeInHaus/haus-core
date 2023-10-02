b14b2040adf1b7a51e2ca5a33c8935aa12e5d854
Claus Wahlers <claus@codeazur.com.br>
2023-10-02 20:29:36 -0300

    Prettier

--- apps/web/components/TexturalVideoDemo/TexturalVideoDemoBasic.tsx
+++ apps/web/components/TexturalVideoDemo/TexturalVideoDemoBasic.tsx
@@ -5,7 +5,7 @@ const TexturalVideoDemoBasic: React.FC = () => {
     <TexturalVideo
       primaryVideoUrl="https://videos.ctfassets.net/j8tkpy1gjhi5/3vr3eDCt4firB4lb5snVrc/c55eb5261efbe9cf0f8489e5aa4b5ab4/Tillamook_Desktop_v7.webm"
       secondaryVideoUrl="https://videos.ctfassets.net/j8tkpy1gjhi5/4YWtNzFROCQBTLFmgt0GtC/46b15d9fbb9a6f5af12819461a37bfc9/Tillamook_Desktop_v7.mp4"
-  />
+    />
   );
 };
 
--- apps/web/components/TexturalVideoDemo/TexturalVideoDemoTransparent.tsx
+++ apps/web/components/TexturalVideoDemo/TexturalVideoDemoTransparent.tsx
@@ -6,8 +6,8 @@ const TexturalVideoDemoTransparent: React.FC = () => {
       primaryVideoUrl="https://happiest.madeinhaus.com/videos/laughs.mp4"
       primaryVideoType='video/mp4; codecs="hvc1"'
       secondaryVideoUrl="https://happiest.madeinhaus.com/videos/laughs.webm"
-      secondaryVideoType='video/webm'
-  />
+      secondaryVideoType="video/webm"
+    />
   );
 };
 
