f13e414e687ee86f00eefc3f41a98378c6704e82
Claus Wahlers <claus@codeazur.com.br>
2023-10-02 20:26:09 -0300

    Update docs

--- apps/web/components/TexturalVideoDemo/TexturalVideoDemoTransparent.tsx
+++ apps/web/components/TexturalVideoDemo/TexturalVideoDemoTransparent.tsx
@@ -3,10 +3,11 @@ import TexturalVideo from '@madeinhaus/textural-video';
 const TexturalVideoDemoTransparent: React.FC = () => {
   return (
     <TexturalVideo
-      mp4="https://happiest.madeinhaus.com/videos/laughs.mp4"
-      webm="https://happiest.madeinhaus.com/videos/laughs.webm"
-      isTransparent
-    />
+      primaryVideoUrl="https://happiest.madeinhaus.com/videos/laughs.mp4"
+      primaryVideoType='video/mp4; codecs="hvc1"'
+      secondaryVideoUrl="https://happiest.madeinhaus.com/videos/laughs.webm"
+      secondaryVideoType='video/webm'
+  />
   );
 };
 
