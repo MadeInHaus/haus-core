43ed27f9cb16db8dbc953a444517b81beac437d2
Ali Klein <ali.klein@madeinhaus.com>
2024-11-28 06:36:11 -0800

    Fix nextra components meta

--- apps/web/pages/components/_meta.js
+++ apps/web/pages/components/_meta.js
@@ -1,30 +1,9 @@
 export default {
-  carousel: {
-    title: 'Carousel',
-    type: 'page'
-  },
-  'contentful-image': {
-    title: 'ContentfulImage',
-    type: 'page'
-  },
-  disclosure: {
-    title: 'Disclosure',
-    type: 'page'
-  },
-  masonry: {
-    title: 'Masonry',
-    type: 'page'
-  },
-  portal: {
-    title: 'Portal',
-    type: 'page'
-  },
-  'textural-video': {
-    title: 'TexturalVideo',
-    type: 'page'
-  },
-  slider: {
-    title: 'Slider',
-    type: 'page'
-  }
+  carousel: 'Carousel',
+  'contentful-image': 'ContentfulImage',
+  disclosure: 'Disclosure',
+  masonry: 'Masonry',
+  portal: 'Portal',
+  'textural-video': 'TexturalVideo',
+  slider: 'Slider',
 };
