5513ea4441d7b9a067ab1048f467a0ae01e1a341
Ali Klein <ali.klein@madeinhaus.com>
2024-12-03 11:10:07 -0800

    Esbuild native module support

--- packages/button/tsup.config.ts
+++ packages/button/tsup.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig, type Options } from 'tsup';
-import { sassPlugin, postcssModules } from 'esbuild-sass-plugin';
 
 export default defineConfig((options: Options) => ({
     entryPoints: ['src/index.tsx'],
     clean: true,
     dts: true,
     format: ['cjs'],
-    esbuildPlugins: [
-        sassPlugin({
-            filter: /\.module\.scss$/,
-            transform: postcssModules({}),
-        }),
-    ],
+    loader: {
+        '.css': 'local-css',
+    },
     ...options,
 }));
